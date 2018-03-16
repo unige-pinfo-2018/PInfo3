dfd9b47272e55edeb43db842d0219a0c154adbca
matteo113 <besancon.matteo@gmail.com>
2018-03-16 15:36:15 +0100

    Finish wildfly prod launch script setup

--- docker-config/wildlfy/run_wildfly.sh
+++ docker-config/wildlfy/run_wildfly.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CONT_NAME="elasticsearch-cozmo"
+CONT_NAME="wildfly-prod"
 
 
 echoerr() { echo "$1" 1>&2; }
@@ -9,4 +9,10 @@ if [[ "$(docker images -q wildfly-prod 2> /dev/null)" == "" ]]; then
         docker build -t wildfly-prod .
 fi
 
-docker run -d -p 8080:8080 -p 9990:9990 -p 8787:8787 -v wildfly-deployments:/opt/jboss/wildfly/standalone/deployments/ wildfly-prod
+
+if [[ "$(docker ps -a -q -f "name=$CONT_NAME" 2> /dev/null)" != "" ]]; then
+        docker start $CONT_NAME
+else
+	docker run -d -p 8080:8080 -p 9990:9990 -p 8787:8787 -v wildfly-deployments:/opt/jboss/wildfly/standalone/deployments/ --name $CONT_NAME wildfly-prod
+
+fi
